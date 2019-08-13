6bb07b2102583f05b4aa48c966542be8f95c5b7a
bgr <bgr.gyk@gmail.com>
2019-08-13 10:56:17 +0200

    Fix BezierCurveUpgrade not upgrading when creating empty curve
    This would cause a bug where upgrade is invoked again later, e.g. on
    next scene load, causing resolution to be changed (to a value that's at
    that point wrong, because user had probably already set the wanted
    value).

--- Assets/BezierCurves/Scripts/BezierCurveUpgrade.cs
+++ Assets/BezierCurves/Scripts/BezierCurveUpgrade.cs
@@ -7,21 +7,25 @@ public static class BezierCurveUpgrade
     public static void Upgrade(BezierCurve curve)
     {
         // resolution logic change - adapt old serialized resolution value to new behavior
-        if (curve.version < 2 && curve.pointCount >= 2)
+        if (curve.version < 2)
         {
-            Debug.Log(string.Format("[BezierCurve upgrade] adapting curve resolution value for '{0}'", curve.name), curve);
-
-            // will use maximum resolution that matches old curve interpolation density
-            float shortestSegmentLength = float.MaxValue;
-            for (int i = 0; i < curve.pointCount - 1; i++)
+            if (curve.pointCount >= 2)
             {
-                float length = BezierCurve.ApproximateLength(curve[i], curve[i + 1], numPoints: 5);
-                if (length < shortestSegmentLength) shortestSegmentLength = length;
-            }
-            float newRes = curve.resolution / shortestSegmentLength;
-            Debug.Log(string.Format("Old resolution: {0}, new resolution: {1}", curve.resolution, newRes), curve);
+                Debug.Log(string.Format("[BezierCurve upgrade] adapting curve resolution value for '{0}'", curve.name), curve);
 
-            curve.resolution = newRes;
+                // will use maximum resolution that matches old curve interpolation density
+                float shortestSegmentLength = float.MaxValue;
+                for (int i = 0; i < curve.pointCount - 1; i++)
+                {
+                    float length = BezierCurve.ApproximateLength(curve[i], curve[i + 1], numPoints: 5);
+                    if (length < shortestSegmentLength) shortestSegmentLength = length;
+                }
+                float newRes = curve.resolution / shortestSegmentLength;
+                Debug.Log(string.Format("Old resolution: {0}, new resolution: {1}", curve.resolution, newRes), curve);
+
+                curve.resolution = newRes;
+            }
+
             curve.version = 2;
         }
     }
